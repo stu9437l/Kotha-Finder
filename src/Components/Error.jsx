9f9ea4d8c390b9da2621d8944707b6b4fad6e4a3
lalbahadurbc <lalbahadurbc2054@gmail.com>
2021-11-05 20:22:25 +0545

    Kotha

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,10 +1,19 @@
  import React from 'react'
+import { useHistory } from 'react-router';
 
 const Error = () => {
+    let history=useHistory();
+    const handleHome=()=>{
+        history.push("/")
+    }
+    
     return (
-        <div>
-            <h1>Error Page</h1>
-        </div>
+      <>
+      <div className="errorpage text-center">
+          <h3 className="text-danger fw-bold text-center"><span className="border-end">404 </span> Page Not Found <span><i class="far fa-grin-tears"></i></span></h3>
+          <button className="btn shadow-lg rounded-0 fw-bold eror_to_home_btn" onClick={handleHome}>Go Home</button>
+      </div>
+      </>
     )
 }
 
--- src/Components/Saved.jsx
+++ src/Components/Saved.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import PasswordShow from './PasswordShow'
-
+import Error from './Error';
 function Saved() {
     return (
         <div>
-            <div className="" style={{marginTop:"120px"}}>
-                <h1>im Saced</h1>
-            </div>
+           
         </div>
     )
 }
